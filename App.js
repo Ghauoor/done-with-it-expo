c2417929c715f63556d235fcaede995e1ce75efb
Ghayoor Hussain <ghayoorhussain701@gmail.com>
2023-08-21 15:30:05 +0500

    Child Navigation using navigation context hook

--- App.js
+++ App.js
@@ -2,15 +2,23 @@ import React from "react";
 import { View, StyleSheet, Text, Button } from "react-native";
 import Screen from "./app/components/Screen";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { NavigationContainer } from "@react-navigation/native";
+import { NavigationContainer, useNavigation } from "@react-navigation/native";
 
+const Link = () => {
+  const navigation = useNavigation();
+  return (
+    <Button
+      title="Go to the TweetsDetails"
+      onPress={() => navigation.navigate("TweetsDetails")}
+    />
+  );
+};
+
+// Tweets Screen
 const Tweets = ({ navigation }) => (
   <Screen>
     <Text>Tweets</Text>
-    <Button
-      title="View Tweets"
-      onPress={() => navigation.navigate("TweetsDetails")}
-    />
+    <Link />
   </Screen>
 );
 
