2eeb654c528d672d6bb86e2fb9fc89305cfc7df8
Ghayoor Hussain <ghayoorhussain701@gmail.com>
2023-08-23 12:37:51 +0500

    error handling

--- app/screens/ListingsScreen.js
+++ app/screens/ListingsScreen.js
@@ -1,26 +1,38 @@
 import React, { useState, useEffect } from "react";
 import { FlatList, StyleSheet } from "react-native";
 
+import Button from "../components/Button";
 import listingApi from "../api/listings";
 import Card from "../components/Card";
 import colors from "../config/colors";
 import Route from "../navigations/Route";
 import Screen from "../components/Screen";
+import AppText from "../components/Text";
 
 function ListingsScreen({ navigation }) {
   const [listings, setListings] = useState([]);
+  const [error, setError] = useState(false);
 
   useEffect(() => {
     loadListings();
   }, []);
   const loadListings = async () => {
     const response = await listingApi.getListings();
+    if (!response.ok) return setError(true);
+
+    setError(false);
     setListings(response.data);
-    console.log(response.data);
   };
   return (
     <Screen style={styles.screen}>
+      {error && (
+        <>
+          <AppText>Couldn't retrieve the listings.</AppText>
+          <Button title="Retry" onPress={loadListings} />
+        </>
+      )}
       <FlatList
+        showsVerticalScrollIndicator={false}
         data={listings}
         keyExtractor={(listing) => listing.id.toString()}
         renderItem={({ item }) => (
