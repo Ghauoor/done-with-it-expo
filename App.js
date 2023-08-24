2c0892024fa9191e664ed0e7a16ad9a5db7f24af
Ghayoor Hussain <ghayoorhussain701@gmail.com>
2023-08-24 18:52:39 +0500

    Setup auth and decode jwt token

--- App.js
+++ App.js
@@ -3,14 +3,17 @@ import { NavigationContainer } from "@react-navigation/native";
 
 import navigationTheme from "./app/navigations/navigationTheme";
 import AppNavigator from "./app/navigations/AppNavigator";
+import AuthNavigator from "./app/navigations/AuthNavigator";
 import OfflineNotice from "./app/components/OfflineNotice";
+import LoginScreen from "./app/screens/LoginScreen";
 
 export default function App() {
   return (
     <>
       <OfflineNotice />
       <NavigationContainer theme={navigationTheme}>
-        <AppNavigator />
+        <AuthNavigator />
+        {/* <AppNavigator /> */}
       </NavigationContainer>
     </>
   );
