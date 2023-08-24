3b423f76d93366d52d586fca9c827b22b6a231ac
Ghayoor Hussain <ghayoorhussain701@gmail.com>
2023-08-24 11:40:03 +0500

    Async Storage

--- App.js
+++ App.js
@@ -1,19 +1,20 @@
 import React from "react";
 import { View, StyleSheet, Text } from "react-native";
-import NetInfo, { useNetInfo } from "@react-native-community/netinfo";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const App = () => {
-  const netInfo = useNetInfo();
+  const demo = async () => {
+    try {
+      await AsyncStorage.setItem("person", JSON.stringify({ id: 1 }));
+      const value = await AsyncStorage.getItem("person");
+      const person = JSON.parse(value);
+      console.log(person);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-  return netInfo.isInternetReachable ? (
-    <View style={{ padding: 44 }}>
-      <Text>Internet is connected</Text>
-    </View>
-  ) : (
-    <View>
-      <Text>Internet is not connected</Text>
-    </View>
-  );
+  demo();
 };
 
 const styles = StyleSheet.create({});
